286c4b72ebef0b3a30868139d6b175881ea18f98
Nícolas Scorzelli <89496417+NScorzelli@users.noreply.github.com>
2022-05-08 15:19:42 -0300

    Update-Routine

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,48 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int _id = 1;
+
+        // CRUD = CREATE - READ - UPDATE - DELETE
+
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            filme.Id = _id++;
+            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(GetFilmeById), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilme()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetFilmeById(int id)
+        {
+            foreach (Filme filme in filmes)
+            {
+                if (filme.Id == id) 
+                {
+                    return Ok(filme);
+                }
+            }
+            return NotFound();
+        } 
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [StringLength(30, ErrorMessage = "O tamanho do título deve ser inferior a 30 caracteres.")]
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+
+        [Range(1,600, ErrorMessage = "A duração do filme deve ser entre 1 minuto e 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
